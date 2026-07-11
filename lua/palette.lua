da7db7460484aacf61b70ea96a7e43ac232f38ec
kytnacode <kytnacode@gmail.com>
2026-07-10 22:41:52 -0700

    Add color palette

---
+++ lua/palette.lua
@@ -0,0 +1,59 @@
+return {
+  -- Pinks
+  deep_pink = '#E92E98',
+  dark_rose = '#F351AD',
+  bright_pink = '#F978C1',
+  marshmellow = '#FCA7D7',
+  cloud_pillow = '#FFE2F2',
+
+  -- Purples
+  dark_amethyst = '#AD31D9',
+  deep_violet = '#C355EA',
+  magic_purple = '#D47BF4',
+  shiny_gem = '#E5A9FA',
+  glowing_purple = '#F7E3FE',
+
+  -- Reds
+  intense_ruby = '#FA3149',
+  flowerfield = '#FC5569',
+  soft_red = '#FC5569',
+  cozy_red = '#FEA8B2',
+  bright_sunshine = '#FFE3E6',
+
+  -- Bases
+  base_50 = '#FAEFF5',
+  base_100 = '#F5DFEA',
+  base_200 = '#F0CFE0',
+  base_300 = '#EBBFD5',
+  base_400 = '#E6AFCB',
+  base_500 = '#B88CA2',
+  base_600 = '#8A697A',
+  base_700 = '#5C4651',
+  base_800 = '#5C4651',
+  base_900 = '#2E2329',
+
+  -- Semantic Colors
+  error = '#FF8B8E',
+  warning = '#FFFB9F',
+  success = '#9AF7A5',
+  info = '#FBB3D8',
+  debug = '#E0B4F8',
+
+  -- ASCII Colors
+  black = '#2E2329',
+  bright_black = '#5C4651',
+  red = '#FF5A5E',
+  bright_red = '#FF8B8E',
+  green = '#6CEC7C',
+  bright_green = '#9AF7A5',
+  yellow = '#FFF858',
+  bright_yellow = '#FFF981',
+  blue = '#958CF1',
+  bright_blue = '#B6B1F0',
+  magenta = '#CB46AA',
+  bright_magenta = '#DE70C3',
+  cyan = '#7DD8ED',
+  bright_cyan = '#B2EBF8',
+  white = '#FAF2F7',
+  bright_white = '#FFFFFF',
+}
