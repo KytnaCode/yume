ee305053e9081c610f8749200333247bd64e3484
kytnacode <kytnacode@gmail.com>
2026-07-10 23:11:02 -0700

    Add devshell

--- flake.nix
+++ flake.nix
@@ -8,11 +8,27 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+
+    yume = pkgs.vimUtils.buildVimPlugin {
+      pname = "yume";
+      version = "0.1.0";
+      src = ./.;
+    };
+
+    neovim-dev = pkgs.neovim.override {
+      configure = {
+        packages.yume = {
+          start = [yume];
+          opt = [];
+        };
+      };
+    };
   in {
     devShells.${system}.default = pkgs.mkShellNoCC {
       buildInputs = with pkgs; [
         git
         just
+        neovim-dev
         stylua
         selene
       ];
