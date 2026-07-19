0502750a9573c4f4a9aa8f1829e2bca89d38f3a4
kytnacode <kytnacode@gmail.com>
2026-07-19 13:54:11 -0700

    Complete lualine theme

--- lua/lualine/themes/yume.lua
+++ lua/lualine/themes/yume.lua
@@ -6,9 +6,29 @@ return {
     b = { bg = colors.white_rose_dust, fg = colors.text },
     c = { bg = colors.alabaster_pink, fg = colors.text },
   },
+  insert = {
+    a = { bg = colors.cream_chiffon, fg = colors.text },
+    b = { bg = colors.chiffon_yellow, fg = colors.text },
+    c = { bg = colors.alabaster_pink, fg = colors.text },
+  },
   visual = {
     a = { bg = colors.psychedelic_purple, fg = colors.alabaster_pink },
     b = { bg = colors.pale_heather, fg = colors.text },
+    c = { bg = colors.alabaster_pink, fg = colors.text },
+  },
+  replace = {
+    a = { bg = colors.bright_turquoise, fg = colors.text },
+    b = { bg = colors.ice_blue, fg = colors.text },
+    c = { bg = colors.alabaster_pink, fg = colors.text },
+  },
+  command = {
+    a = { bg = colors.hint_of_mint, fg = colors.text },
+    b = { bg = colors.pale_pistachio, fg = colors.text },
+    c = { bg = colors.alabaster_pink, fg = colors.text },
+  },
+  inactive = {
+    a = { bg = colors.muted_eggplant, fg = colors.alabaster_pink },
+    b = { bg = colors.pave_mauve_mist, fg = colors.text },
     c = { bg = colors.alabaster_pink, fg = colors.text },
   },
 }
