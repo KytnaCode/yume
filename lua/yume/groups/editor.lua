4da0fb820e7a623d621efbc52b721dec5bc4dd94
kytnacode <kytnacode@gmail.com>
2026-07-19 16:49:24 -0700

    Set status line color

--- lua/yume/groups/editor.lua
+++ lua/yume/groups/editor.lua
@@ -6,6 +6,7 @@ M.setup = function()
   return {
     Cursor = { fg = colors.alabaster_pink, bg = colors.carnation_pink },
     CursorLine = { bg = colors.white_rose_dust },
+    StatusLine = { bg = colors.alabaster_pink },
     Normal = { fg = colors.text, bg = colors.alabaster_pink },
     Visual = { fg = colors.text, bg = colors.pastel_lavender },
     VisualNOS = { fg = colors.text, bg = colors.pale_thistle },
