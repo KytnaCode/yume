efdf639af220d9158e3d923956978ba0e20bb68d
kytnacode <kytnacode@gmail.com>
2026-07-19 13:04:02 -0700

    Configure test neovim config

--- flake.nix
+++ flake.nix
@@ -17,6 +17,18 @@
 
     neovim-dev = pkgs.neovim.override {
       configure = {
+        customRC = ''
+          lua << EOF
+          vim.o.tabstop = 2
+          vim.o.expandtab = true
+          vim.o.termguicolors = true
+          vim.o.shiftwidth = 2
+          vim.o.number = true
+          vim.o.relativenumber = true
+          vim.o.signcolumn = 'yes:1'
+          EOF
+        '';
+
         packages.yume = {
           start = [yume];
           opt = [];
