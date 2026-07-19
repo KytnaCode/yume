2fdf5d1992b90f60a7fb87aad215436a9d9d4a5b
kytnacode <kytnacode@gmail.com>
2026-07-19 14:07:38 -0700

    Configure lualine

--- flake.nix
+++ flake.nix
@@ -27,7 +27,25 @@
           vim.o.relativenumber = true
           vim.o.signcolumn = 'yes:1'
 
-          require('lualine').setup({ options = { theme = "yume" } })
+          require('lualine').setup({
+            options = {
+              theme = "yume",
+              component_separators = '|',
+              section_separators = { left = '', right = '' },
+            },
+            sections = {
+              lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+              lualine_b = { 'filename', 'branch' },
+              lualine_c = {
+                '%=', --[[ add your center components here in place of this comment ]]
+              },
+              lualine_x = {},
+              lualine_y = { 'filetype', 'progress' },
+              lualine_z = {
+                { 'location', separator = { right = '' }, left_padding = 2 },
+              },
+            },
+          })
           EOF
         '';
 
