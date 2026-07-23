acef68eb6066b4e756d51e63cc75eee5fb7e8b1b
kytnacode <kytnacode@gmail.com>
2026-07-23 08:34:33 -0700

    Create plugin package

--- flake.nix
+++ flake.nix
@@ -6,92 +6,107 @@
   };
 
   outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    systems = ["x86_64-linux"];
+    eachSystem = f: nixpkgs.genAttrs systems (system: f import nixpkgs {inherit system;});
 
-    yume = pkgs.vimUtils.buildVimPlugin {
-      pname = "yume";
-      version = "0.1.0";
-      src = ./.;
-    };
+    get-yume = pkgs:
+      pkgs.vimUtils.buildVimPlugin {
+        pname = "yume";
+        version = "0.1.0";
+        src = ./.;
+      };
 
-    neovim-dev = pkgs.neovim.override {
-      configure = {
-        customRC = ''
-          lua << EOF
-          vim.o.tabstop = 2
-          vim.o.expandtab = true
-          vim.o.termguicolors = true
-          vim.o.shiftwidth = 2
-          vim.o.number = true
-          vim.o.relativenumber = true
-          vim.o.signcolumn = 'yes:1'
+    get-neovim = pkgs:
+      pkgs.neovim.override {
+        configure = {
+          customRC = ''
+            lua << EOF
+            vim.o.tabstop = 2
+            vim.o.expandtab = true
+            vim.o.termguicolors = true
+            vim.o.shiftwidth = 2
+            vim.o.number = true
+            vim.o.relativenumber = true
+            vim.o.signcolumn = 'yes:1'
 
-          require('lualine').setup({
-            options = {
-              theme = "yume",
-              component_separators = '|',
-              section_separators = { left = '', right = '' },
-            },
-            sections = {
-              lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-              lualine_b = { 'filename', 'branch' },
-              lualine_c = {
-                '%=', --[[ add your center components here in place of this comment ]]
+            require('lualine').setup({
+              options = {
+                theme = "yume",
+                component_separators = '|',
+                section_separators = { left = '', right = '' },
               },
-              lualine_x = {},
-              lualine_y = { 'filetype', 'progress' },
-              lualine_z = {
-                { 'location', separator = { right = '' }, left_padding = 2 },
+              sections = {
+                lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+                lualine_b = { 'filename', 'branch' },
+                lualine_c = {
+                  '%=', --[[ add your center components here in place of this comment ]]
+                },
+                lualine_x = {},
+                lualine_y = { 'filetype', 'progress' },
+                lualine_z = {
+                  { 'location', separator = { right = '' }, left_padding = 2 },
+                },
               },
-            },
-          })
+            })
 
-          require('neo-tree').setup({})
+            require('neo-tree').setup({})
 
-          require('yume').setup({
-            integrations = {
-              neotree = true,
-            },
-          })
+            require('yume').setup({
+              integrations = {
+                neotree = true,
+              },
+            })
 
-          vim.cmd('colorscheme yume')
-          EOF
-        '';
+            vim.cmd('colorscheme yume')
+            EOF
+          '';
 
-        packages = {
-          yume = {
-            start = [yume];
-            opt = [];
-          };
-          nvim-web-dev-icons = {
-            start = [pkgs.vimPlugins.nvim-web-devicons];
-          };
-          nui = {
-            start = [pkgs.vimPlugins.nui-nvim];
-          };
-          plenary = {
-            start = [pkgs.vimPlugins.plenary-nvim];
-          };
-          neo-tree = {
-            start = [pkgs.vimPlugins.neo-tree-nvim];
-          };
-          lualine = {
-            start = [pkgs.vimPlugins.lualine-nvim];
-            opt = [];
+          packages = {
+            yume = {
+              start = [get-yume pkgs];
+              opt = [];
+            };
+            nvim-web-dev-icons = {
+              start = [pkgs.vimPlugins.nvim-web-devicons];
+            };
+            nui = {
+              start = [pkgs.vimPlugins.nui-nvim];
+            };
+            plenary = {
+              start = [pkgs.vimPlugins.plenary-nvim];
+            };
+            neo-tree = {
+              start = [pkgs.vimPlugins.neo-tree-nvim];
+            };
+            lualine = {
+              start = [pkgs.vimPlugins.lualine-nvim];
+              opt = [];
+            };
           };
         };
       };
-    };
   in {
-    devShells.${system}.default = pkgs.mkShellNoCC {
-      buildInputs = with pkgs; [
-        git
-        just
-        neovim-dev
-        stylua
-        selene
-      ];
-    };
+    devShells = eachSystem (pkgs: {
+      default = let
+        neovim-dev = get-neovim pkgs;
+      in
+        pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            git
+            just
+            neovim-dev
+            stylua
+            selene
+          ];
+        };
+    });
+
+    packages = eachSystem (pkgs: let
+      yume = get-yume pkgs;
+    in {
+      inherit yume;
+
+      default = yume;
+    });
   };
 }
