1c97577e58bccc19f3c8f10bd5d19d7a3aea136f
kytnacode <kytnacode@gmail.com>
2026-07-23 05:52:44 -0700

    Set function

--- lua/yume/groups/treesitter.lua
+++ lua/yume/groups/treesitter.lua
@@ -8,7 +8,8 @@ M.setup = function()
     ['@variable.builtin'] = { fg = colors.bright_orchid },
     ['@variable.parameter'] = { fg = colors.neon_orchid },
     ['@string'] = { fg = colors.carnation_pink },
-    ['@function.builtin'] = { link = 'Keyword' },
+    ['@function.builtin'] = { link = '@function' },
+    ['@function'] = { link = 'Keyword' },
   }
 end
 
